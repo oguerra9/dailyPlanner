79d569485d7384320f98bd42ab666244b990f62e
olg0002 <olg0002@auburn.edu>
2023-05-23 14:38:06 -0400

    beginning to integrate global states

--- client/src/App.js
+++ client/src/App.js
@@ -3,46 +3,49 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PlannerContainer from "./pages/PlannerContainer";
 import NavBar from "./components/NavBar";
 import Settings from './pages/Settings';
+import { PlannerProvider } from './utils/PlannerContext';
 
 function App() {
     return (
         <Router>
             <div className="flex-column justify-center align-center min-100-vh bg-primary">
-                <NavBar />
-                <Routes>
-                    <Route 
-                        path="/" 
-                        element={<PlannerContainer view={'day'}/>}
-                    />
-                    <Route 
-                        path="/day" 
-                        element={<PlannerContainer view={'day'}/>}
-                    />
-                    <Route 
-                        path="/day/:timestamp" 
-                        element={<PlannerContainer view={'day'}/>}
-                    />
-                    <Route 
-                        path="/week" 
-                        element={<PlannerContainer view={'week'}/>}
-                    />
-                    <Route 
-                        path="/week/:timestamp" 
-                        element={<PlannerContainer view={'week'}/>}
-                    />
-                    <Route 
-                        path="/month" 
-                        element={<PlannerContainer view={'month'}/>}
-                    />
-                    <Route 
-                        path="/month/:timestamp" 
-                        element={<PlannerContainer view={'month'}/>}
-                    />
-                    <Route 
-                        path="/settings" 
-                        element={<Settings />}
-                    />
-                </Routes>
+                <PlannerProvider>
+                    <NavBar />
+                    <Routes>
+                        <Route 
+                            path="/" 
+                            element={<PlannerContainer propView={'day'}/>}
+                        />
+                        <Route 
+                            path="/day" 
+                            element={<PlannerContainer propView={'day'}/>}
+                        />
+                        <Route 
+                            path="/day/:timestamp" 
+                            element={<PlannerContainer propView={'day'}/>}
+                        />
+                        <Route 
+                            path="/week" 
+                            element={<PlannerContainer propView={'week'}/>}
+                        />
+                        <Route 
+                            path="/week/:timestamp" 
+                            element={<PlannerContainer propView={'week'}/>}
+                        />
+                        <Route 
+                            path="/month" 
+                            element={<PlannerContainer propView={'month'}/>}
+                        />
+                        <Route 
+                            path="/month/:timestamp" 
+                            element={<PlannerContainer propView={'month'}/>}
+                        />
+                        <Route 
+                            path="/settings" 
+                            element={<Settings />}
+                        />
+                    </Routes>
+                </PlannerProvider>
             </div>
         </Router>
     );
---
+++ client/src/utils/PlannerContext.js
@@ -0,0 +1,45 @@
+import React, { useState, createContext, useContext } from "react";
+import DataService from '../services/dataService';
+
+
+const PlannerContext = createContext();
+const { Provider } = PlannerContext;
+
+const PlannerProvider = ({ children })  => {
+
+    const [view, setView] = useState('day');
+
+    const changeView = (viewStr) => {
+        setView(viewStr);
+    };
+
+    const [timestamp, setTimestamp] = useState((new Date()).getTime());
+
+    const changeTimestamp = (newTime) => {
+        setTimestamp(newTime);
+        setTSDate(new Date(newTime));
+    };
+
+    const [TSDate, setTSDate] = useState(new Date(timestamp));
+
+    const changeTSDate = (date) => {
+        setTimestamp(date.getTime());
+        setTSDate(date);
+    };
+
+    return (
+        <PlannerContext.Provider
+            value={{ view, changeView, timestamp, changeTimestamp, TSDate, changeTSDate }}
+        >
+            {/* We render children in our component so that any descendent can access the value from the provider */}
+            {children}
+        </PlannerContext.Provider>
+    );
+};
+
+const usePlannerContext = () => {
+  return useContext(PlannerContext);
+};
+
+export { PlannerProvider, usePlannerContext };
+
